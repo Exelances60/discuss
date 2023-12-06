8133831ac2a3fee4e41b597350a9297c8ad389da
Enes Çelik <enescelik6012@gmail.com>
2023-12-06 11:54:35 +0300

    Database query cache  every show page

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,15 +1,16 @@
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 import { db } from "@/db";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export const fetchCommentsByPostId = async (
-  postId: string
-): Promise<CommentWithAuthor[]> => {
-  return db.comment.findMany({
-    where: { postId },
-    include: { user: { select: { name: true, image: true } } },
-  });
-};
+export const fetchCommentsByPostId = cache(
+  async (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: { postId },
+      include: { user: { select: { name: true, image: true } } },
+    });
+  }
+);
